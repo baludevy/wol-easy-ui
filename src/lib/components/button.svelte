1d05a5ea478a5d77f6ff5db4c8e0d6272ed1915a
balu <babjakbalazs5@gmail.com>
2024-11-20 20:21:54 +0100

    unnecessary button stuff

--- src/lib/components/button.svelte
+++ src/lib/components/button.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <button
-	class="bg-base font-medium rounded-lg text-sm px-3.5 py-3.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+	class="bg-base font-medium rounded-lg text-sm px-3.5 py-3.5 me-2 mb-2"
 	on:click={onClick}
 >
 	<slot />
